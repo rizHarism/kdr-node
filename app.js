6f684de51633bb77cd34830f2f63e7ccbb870a57
rizHarism <riz.harism@gmail.com>
2024-08-06 19:04:50 +0700

    add condition in /general if no data response

--- app.js
+++ app.js
@@ -1,66 +1,68 @@
-const express = require('express')
-const app = express()
-require('./utils/db')
-const Product = require('./model/products')
-const Partner = require('./model/partners')
-const General = require('./model/general')
-const About = require('./model/about')
-const { uploadHandler } = require("./utils/multer-config")
-const fs = require('fs')
-const port = 8080
+const express = require("express");
+const app = express();
+require("./utils/db");
+const Product = require("./model/products");
+const Partner = require("./model/partners");
+const General = require("./model/general");
+const About = require("./model/about");
+const { uploadHandler } = require("./utils/multer-config");
+const fs = require("fs");
+const port = 80;
 
 // built in middleware
-app.use(express.static('public'))
-// application level middleware 
+app.use(express.static("public"));
+// application level middleware
 app.use((req, res, next) => {
-  console.log('Time:', Date.now())
-  next()
-})
+  console.log("Time:", Date.now());
+  next();
+});
 
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
-    message: "hello world !"
+    message: "hello world !",
   });
-})
+});
 
-app.get('/general', (req, res) => {
+app.get("/general", (req, res) => {
   General.find().then((general) => {
-    res.status(200).json(general)
-  })
-})
+    if (general.length > 0) {
+      res.status(200).json(general);
+    } else {
+      res.status(404).send("data not found");
+    }
+  });
+});
 
-app.get('/about', (req, res) => {
+app.get("/about", (req, res) => {
   About.find().then((about) => {
-    res.status(200).json(about)
-  })
-})
+    res.status(200).json(about);
+  });
+});
 
-app.get('/products', (req, res) => {
+app.get("/products", (req, res) => {
   Product.find().then((product) => {
-    res.status(200).json(product)
-  })
-})
+    res.status(200).json(product);
+  });
+});
 
-app.get('/partners', (req, res) => {
+app.get("/partners", (req, res) => {
   Partner.find().then((partner) => {
-    res.status(200).json(partner)
-  })
-})
+    res.status(200).json(partner);
+  });
+});
 
-app.post('/user', uploadHandler.single('image'), (req, res) => {
-  console.log(req.file, req.body)
-  res.status(200)
-  res.send(req.body)
-})
+app.post("/user", uploadHandler.single("image"), (req, res) => {
+  console.log(req.file, req.body);
+  res.status(200);
+  res.send(req.body);
+});
 
 // response 404
 app.use((req, res) => {
-  res.status(404)
-  res.send("404 not found")
-})
-
+  res.status(404);
+  res.send("404 not found");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
