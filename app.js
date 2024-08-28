10397a6554f780c9ad81211e9bfa31849a5c2214
rizHarism <riz.harism@gmail.com>
2024-08-28 13:33:42 +0700

    disable cors origin as comment

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://localhost:3000", "https://kartadayareksabumi.com"],
+    // origin: ["https://localhost:3000", "https://kartadayareksabumi.com"],
     credentials: true,
   })
 );
